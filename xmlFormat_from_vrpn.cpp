4a150bc83a75e59333afdd853fd5195e38356515
Mohammad Jahed Murad Sunny <64752337+juvenileSunny@users.noreply.github.com>
2023-03-28 14:12:49 -0500

    XML_from_vrpn
    To clarify, the code provided earlier is a partial implementation that includes the basic functionality for getting position and button data from VRPN devices and storing that data in an XML format. However, there may be additional work needed to make the code fully functional and suitable for specific use cases.
    
    For instance, the current implementation only saves data to an XML file, but it may be necessary to modify the code to include additional functionality such as transmitting data over a network, using a different file format, or handling errors that may arise during data acquisition or storage.
    
    In summary, while the code provides a starting point for accessing and storing VRPN data in an XML format, it may require additional modifications and testing to be fully functional and suitable for specific applications.

---
+++ xmlFormat_from_vrpn.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include "vrpn_Tracker.h"
+#include "vrpn_Button.h"
+#include "tinyxml2.h"
+
+using namespace std;
+using namespace tinyxml2;
+
+void VRPN_CALLBACK handle_tracker_position(void* userData, const vrpn_TRACKERCB t)
+{
+    XMLDocument doc;
+    XMLNode* rootNode = doc.NewElement("tracker_position");
+    doc.InsertFirstChild(rootNode);
+    XMLElement* sensorNode = doc.NewElement("sensor");
+    sensorNode->SetText(t.sensor);
+    rootNode->InsertEndChild(sensorNode);
+    XMLElement* xNode = doc.NewElement("x");
+    xNode->SetText(t.pos[0]);
+    rootNode->InsertEndChild(xNode);
+    XMLElement* yNode = doc.NewElement("y");
+    yNode->SetText(t.pos[1]);
+    rootNode->InsertEndChild(yNode);
+    XMLElement* zNode = doc.NewElement("z");
+    zNode->SetText(t.pos[2]);
+    rootNode->InsertEndChild(zNode);
+    doc.SaveFile("tracker_positions.xml");
+}
+
+void VRPN_CALLBACK handle_button_press(void* userData, const vrpn_BUTTONCB b)
+{
+    XMLDocument doc;
+    XMLNode* rootNode = doc.NewElement("button_press");
+    doc.InsertFirstChild(rootNode);
+    XMLElement* buttonNode = doc.NewElement("button");
+    buttonNode->SetText(b.button);
+    rootNode->InsertEndChild(buttonNode);
+    XMLElement* stateNode = doc.NewElement("state");
+    stateNode->SetText(b.state == 1 ? "pressed" : "released");
+    rootNode->InsertEndChild(stateNode);
+    doc.SaveFile("button_presses.xml");
+}
+
+int main()
+{
+    vrpn_Tracker_Remote* tracker = new vrpn_Tracker_Remote("Tracker0@localhost");
+    tracker->register_change_handler(NULL, handle_tracker_position);
+
+    vrpn_Button_Remote* button = new vrpn_Button_Remote("Button0@localhost");
+    button->register_change_handler(NULL, handle_button_press);
+
+    while (true)
+    {
+        tracker->mainloop();
+        button->mainloop();
+        // Sleep for a short period to avoid hogging the CPU
+        vrpn_SleepMsecs(10);
+    }
+
+    return 0;
+}
